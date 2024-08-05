d947bcaf6b7a5e533bd877126e8877b8fb20a9fe
JulitaKołodziejczyk <julitakolodziejczyk1995@gmail.com>
2024-08-05 12:23:52 +0200

    followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    if @user == current_user
+      @following_users = current_user.following
+      @followers = current_user.followers
+    else
+      @following_users = @user.following
+      @followers = @user.followers
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
   
-  resources :users, only: [:show]
-  
-  
+  resources :users, only: [:show] do
+    resources :relationships, only: [:create, :destroy]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
