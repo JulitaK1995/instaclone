56ed050c8a2ee772cd1dbc5500224c21b79ab4e9
JulitaKołodziejczyk <julitakolodziejczyk1995@gmail.com>
2024-08-06 10:56:17 +0200

    addsearch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def search
+    @query = params[:query]
+    if @query.present?
+      @users = User.where("username LIKE ?", "%#{@query}%")
+    else
+      @users = User.none
+    end
+    render :index
+  end
+  
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
